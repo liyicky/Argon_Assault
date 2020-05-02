a7928977da42c5eccac7199c30d349a1bac64027
Jason Colby Cheladyn <nirinth@gmail.com>
2020-05-02 10:40:29 +0900

    circuit

---
+++ Assets/SplashController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("loadScene", 2);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        
+    }
+
+    void loadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
